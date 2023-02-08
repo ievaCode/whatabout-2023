0f2e5e9f9aa945a2393def9cf1dbcf1b3b3c44a9
ievaCode <iewute@yahoo.com>
2023-02-08 11:46:16 +0200

    registration form added. Corrections on Login form. Form input validation. No submit logics yet.

--- src/components/registration-layout/Login.jsx
+++ src/components/registration-layout/Login.jsx
@@ -16,14 +16,9 @@ const Login = () => {
     email: Yup.string()
       .email('This should be a valid e-mail.')
       .required('This field must be filled.'),
-    username: Yup.string()
-      .min(2,'Username shoudl contain at least two letters.')
-      .required('This field must be filled.'), 
     password: Yup.string()
       .min(8, 'Password must be at least 8 symbols length.')
       .required('This field must be filled.'),
-    imageUrl: Yup.string()
-      .url('This shoudl be an actual url')
   });
 
   return (
@@ -48,16 +43,6 @@ const Login = () => {
               </label>
             </div>
             <div>
-              <label>Username:
-                <Field 
-                  name='username'
-                  value={values.username} 
-                  onChange={(e)=>setValues({...values, username:e.target.value})}
-                />
-                {errors.username && touched.username ? <span>{errors.username}</span> : null}
-              </label>
-            </div>
-            <div>
               <label>Password:
                 <Field name='password'
                   type='password'
@@ -67,16 +52,6 @@ const Login = () => {
                 {errors.password && touched.password ? <span>{errors.password}</span> : null}
               </label>
             </div>            
-            <div>
-              <label> Image url:
-                <Field name='imageUrl'
-                  type='url'
-                  value={values.imageUrl}
-                  onChange={(e)=>setValues({...values, imageUrl:e.target.value})}
-                />
-                { errors.imageUrl && touched.imageUrl ? <span>{errors.imageUrl}</span> : null }
-              </label>
-            </div>
             <button type="submit">Submit</button>
           </Form>
         )}
--- src/components/registration-layout/Register.jsx
+++ src/components/registration-layout/Register.jsx
@@ -1,10 +1,101 @@
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { useState } from 'react';
 
 const Register = () => {
 
+  //cia gal nereikia steito, o tik kintamojo
+  const [formInputs, setFormInputs] = useState({
+    email: '',
+    username: '',
+    imageUrl: '',
+    password1: '',
+    password2: '',
+  });
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('This should be a valid e-mail.')
+      .required('This field must be filled.'),
+    username: Yup.string()
+      .min(2,'Username shoudl contain at least two letters.')
+      .required('This field must be filled.'),
+      imageUrl: Yup.string()
+      .url('This shoudl be an actual url') ,
+    password1: Yup.string()
+      .min(8, 'Password must be at least 8 symbols length.')
+      .required('This field must be filled.'),
+    password2: Yup.string()
+      .min(8, 'Password must be at least 8 symbols length.')
+      .required('This field must be filled.')
+  });
+
   return (
-    <main className="registration">
-      Registration
-    </main>
+    <div className="registration">
+      <Formik
+        initialValues={formInputs}
+        validationSchema={validationSchema}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched, /*isValidating, validateField, validateFormvalues, */ values, setValues }) => (
+          <Form>
+            <div>
+              <label>Email:
+                <Field 
+                  name='email'
+                  value={values.email} 
+                  onChange={(e)=>setValues({...values, email:e.target.value})}
+                />
+                {errors.email && touched.email ? <span>{errors.email}</span> : null}
+              </label>
+            </div>
+            <div>
+              <label>Username:
+                <Field 
+                  name='username'
+                  value={values.username} 
+                  onChange={(e)=>setValues({...values, username:e.target.value})}
+                />
+                {errors.username && touched.username ? <span>{errors.username}</span> : null}
+              </label>
+            </div>
+            <div>
+              <label> Image url:
+                <Field name='imageUrl'
+                  type='url'
+                  value={values.imageUrl}
+                  onChange={(e)=>setValues({...values, imageUrl:e.target.value})}
+                />
+                { errors.imageUrl && touched.imageUrl ? <span>{errors.imageUrl}</span> : null }
+              </label>
+            </div>
+            <div>
+              <label>Password:
+                <Field name='password1'
+                  type='password'
+                  value={values.password1} 
+                  onChange={(e)=>setValues({...values, password1:e.target.value})}
+                />
+                {errors.password1 && touched.password1 ? <span>{errors.password1}</span> : null}
+              </label>
+            </div>
+            <div>
+              <label>Repeat password:
+                <Field name='password2'
+                  type='password'
+                  value={values.password2} 
+                  onChange={(e)=>setValues({...values, password2:e.target.value})}
+                />
+                {errors.password2 && touched.password2 ? <span>{errors.password2}</span> : null}
+              </label>
+            </div>               
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
  
