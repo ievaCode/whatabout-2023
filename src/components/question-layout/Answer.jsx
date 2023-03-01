463b22a244888e9272656fd72b454c7e11dde2c1
ievaCode <iewute@yahoo.com>
2023-03-02 01:09:08 +0200

    questionPage reload issue on refresh solved

--- src/components/question-layout/Answer.jsx
+++ src/components/question-layout/Answer.jsx
@@ -10,63 +10,46 @@ const Question = ({answer, tagNumber}) => {
 
   const { deleteAnswer, updateAnswer } = useContext(AnswerContext);
   const { users, loggedInUser } = useContext(UserContext);
-  const answerOwner = users.find(user => user.id === answer.authorId);
   
   return (
     <article className="card answerCard">
-        <div className="answerTag">Answer {tagNumber}</div>
-        <div className="topSectionContainer">
-            <div className="dateContainer">
-                <span className="date">posted: {answer.date}</span>
-                {answer.edited && <span className="date edited">last edited on: {answer.edited}</span>}
-            </div>
-            {loggedInUser && loggedInUser.id === answerOwner.id ?
-                <div className="manageButtonsWrapper">
-                    <div className="button editButton"><Link to={`/questions/edit-answer/${answer.id}`}>Edit</Link></div>
-                    <button className="button deleteButton" onClick = {()=>deleteAnswer(answer.id)}>Delete</button>          
-                </div>  :
-                <UserCard userData = {answerOwner}/>      
-            }    
-        </div>
-        <div className="mainSectionContainer">
-            <div className="leftSideContainer">                
-                {loggedInUser? 
-                    <LikeCount item={answer} action={updateAnswer} /> :
-                    <div className="statistics">
-                        <p className="rateWrapper">rate: 
-                            <span className="rate"> {answer.likedBy.length-answer.dislikedBy.length}</span>
-                        </p>
-                        <p className="votes">from total votes of {answer.likedBy.length+answer.dislikedBy.length}</p>
-                    </div>
-                }
-            </div>
-            <div className="rightSideContainer">
-                <p className="answerText questionText">{answer.answer}</p>   
-            </div>
-        </div>
-
-        
-        {/* <UserCard userData = {answerOwner}/>
-        {loggedInUser && loggedInUser.id === answerOwner.id &&
+        {users ?
             <>
-                <div className="button editButton"><Link to={`/questions/edit-answer/${answer.id}`}>Edit my answer</Link></div>
-                <button className="button deleteButton" onClick = {()=> deleteAnswer(answer.id)}>Delete my answer</button>                
-            </>        
-        }
-        <span className="date">{answer.date}</span>
-        {answer.edited && <span className="edited">last edited on {answer.edited}</span>} */}
-        {/* {loggedInUser? 
-            <LikeCount item={answer} action={updateAnswer} /> :
-            <div className="statistics">                
-                <span className="rate">Rate: {answer.likedBy.length-answer.dislikedBy.length}</span> 
-                <span className="votes">Votes: {answer.likedBy.length+answer.dislikedBy.length}</span>               
+            <div className="answerTag">Answer {tagNumber}</div>
+            <div className="topSectionContainer">
+                <div className="dateContainer">
+                    <span className="date">posted: {answer.date}</span>
+                    {answer.edited && <span className="date edited">last edited on: {answer.edited}</span>}
+                </div>
+                {loggedInUser && loggedInUser.id === users.find(user => user.id === answer.authorId).id ?
+                    <div className="manageButtonsWrapper">
+                        <div className="button editButton">
+                            <Link to={`/questions/edit-answer/${answer.id}`}>Edit</Link>
+                        </div>
+                        <button className="button deleteButton" onClick = {()=>deleteAnswer(answer.id)}>Delete</button>          
+                    </div>  :
+                    <UserCard userData = {users.find(user => user.id === answer.authorId)}/>      
+                }    
             </div>
-        }
-        <div className="mainSection"> 
-            <p className="answerText">{answer.answer}</p>      
-        </div>  */}
-    </article>
-  );
+            <div className="mainSectionContainer">
+                <div className="leftSideContainer">                
+                    {loggedInUser? 
+                        <LikeCount item={answer} action={updateAnswer} /> :
+                        <div className="statistics">
+                            <p className="rateWrapper">rate: 
+                                <span className="rate"> {answer.likedBy.length-answer.dislikedBy.length}</span>
+                            </p>
+                            <p className="votes">from total votes of {answer.likedBy.length+answer.dislikedBy.length}</p>
+                        </div>
+                    }
+                </div>
+                <div className="rightSideContainer">
+                    <p className="answerText questionText">{answer.answer}</p>   
+                </div>
+            </div>
+            </>: <p>Loading..</p> }
+        </article>
+  )
 }
  
 export default Question;
--- src/components/question-layout/Question.jsx
+++ src/components/question-layout/Question.jsx
@@ -17,57 +17,55 @@ const Question = ({question}) => {
 
   const navigation = useNavigate();
 
-  const questionOwner = users.find(user => user.id === question.authorId);
-
-  const numberOfAnswers = answers.filter(answer => answer.questionId === question.id).length;
-
   const onDeleteQuestion = (id) => {
     deleteQuestion(id);
-    // const relevantAnswers = answers.filter(answer => answer.questionId === id);
-    // relevantAnswers.forEach(answer => deleteAnswer(answer.id));
     navigation('/questions');  
   }
 
   return (
     <article className="card questionCard">
-
-        <div className="topSectionContainer">
-            <div className="dateContainer">
-                <span className="date">posted: {question.date}</span>
-                {question.edited && <span className="date edited">last edited on: {question.edited}</span>}
-            </div>
-            {loggedInUser && loggedInUser.id === questionOwner.id ?
-                <div className="manageButtonsWrapper">
-                    <div className="button editButton"><Link to={`/questions/edit-question/${question.id}`}>Edit</Link></div>
-                    <button className="button deleteButton" onClick = {()=>onDeleteQuestion(question.id)}>Delete</button>          
-                </div>  :
-                <UserCard userData = {questionOwner}/>      
-            }    
-        </div>
-        
-        <div className="mainSectionContainer">
-            <div className="leftSideContainer">                
-                {loggedInUser? 
-                    <LikeCount item={question} action={updateQuestion} /> :
-                    <div className="statistics">
-                        <p className="rateWrapper">rate: 
-                            <span className="rate"> {question.likedBy.length-question.dislikedBy.length}</span>
-                        </p>
-                        <p className="votes">from total votes of {question.likedBy.length+question.dislikedBy.length}</p>
+        {users && question?
+            <>
+                <div className="topSectionContainer">
+                    <div className="dateContainer">
+                        <span className="date">posted: {question.date}</span>
+                        {question.edited && <span className="date edited">last edited on: {question.edited}</span>}
                     </div>
-                }
-            </div>
-            <div className="rightSideContainer">
-                <Link className="questionHeading" to={`/questions/${question.id}`}>{question.question}</Link>
-                {loggedInUser && <p className="questionText">{question.explanation}</p> }                
-            </div>
-            <div className="answerButton numberOfAnswers">{numberOfAnswers} answers</div>
-            {loggedInUser &&
-                <div className="answerButton addAnswer">
-                    <Link to={`/questions/${question.id}/new-answer`}>Add Your answer</Link>
+                    {loggedInUser && loggedInUser.id === users.find(user => user.id === question.authorId).id ?
+                        <div className="manageButtonsWrapper">
+                            <div className="button editButton"><Link to={`/questions/edit-question/${question.id}`}>Edit</Link></div>
+                            <button className="button deleteButton" onClick = {()=>onDeleteQuestion(question.id)}>Delete</button>          
+                        </div>  :
+                        <UserCard userData = {users.find(user => user.id === question.authorId)}/>      
+                    }    
                 </div>
-            }
-        </div>
+                
+                <div className="mainSectionContainer">
+                    <div className="leftSideContainer">                
+                        {loggedInUser? 
+                            <LikeCount item={question} action={updateQuestion} /> :
+                            <div className="statistics">
+                                <p className="rateWrapper">rate: 
+                                    <span className="rate"> {question.likedBy.length-question.dislikedBy.length}</span>
+                                </p>
+                                <p className="votes">from total votes of {question.likedBy.length+question.dislikedBy.length}</p>
+                            </div>
+                        }
+                    </div>
+                    <div className="rightSideContainer">
+                        <Link className="questionHeading" to={`/questions/${question.id}`}>{question.question}</Link>
+                        {loggedInUser && <p className="questionText">{question.explanation}</p> }                
+                    </div>
+                    <div className="answerButton numberOfAnswers">{answers.filter(answer => answer.questionId === question.id).length} answers</div>
+                    {loggedInUser &&
+                        <div className="answerButton addAnswer">
+                            <Link to={`/questions/${question.id}/new-answer`}>Add Your answer</Link>
+                        </div>
+                    }
+                </div>
+            </> : 
+            <p>Loading..</p>
+        }
     </article>
   );
 }
